4a42ff419d207bcf1d99557dbef94904c72f3ee2
Geovanna Otoni <geovannaotoni@gmail.com>
2023-11-24 15:24:30 -0300

    feat02: classe PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,31 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * The type Pessoa candidata.
+ */
+public class PessoaCandidata extends Pessoa {
+  protected int numero;
+  protected int votos;
 
+  public PessoaCandidata(String nome, int numero) {
+    setNome(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
