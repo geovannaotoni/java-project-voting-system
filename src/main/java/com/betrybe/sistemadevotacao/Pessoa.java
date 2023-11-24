c1b6c7478c758ba7216f23ff0386868aa76d0265
Geovanna Otoni <geovannaotoni@gmail.com>
2023-11-24 15:11:15 -0300

    feat01: classe abstrata Pessoa

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,16 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+/**
+ * The type Pessoa.
+ */
+public abstract class Pessoa {
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
