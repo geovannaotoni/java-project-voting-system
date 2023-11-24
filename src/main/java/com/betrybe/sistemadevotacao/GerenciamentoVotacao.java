fb9aba4d2067561910e93793a9a4ce985961c995
Geovanna Otoni <geovannaotoni@gmail.com>
2023-11-24 16:24:36 -0300

    feat04: classe GerenciamentoVotacao com atributos

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,8 +1,32 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.ArrayList;
+
 /**
  * The type Gerenciamento votacao.
  */
-public class GerenciamentoVotacao {
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfsComputados;
 
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
