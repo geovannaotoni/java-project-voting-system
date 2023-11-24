3eb047bd0bc395be73325c3013ee1c60884d8949
Geovanna Otoni <geovannaotoni@gmail.com>
2023-11-24 15:49:52 -0300

    feat03: classe PessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -3,6 +3,25 @@ package com.betrybe.sistemadevotacao;
 /**
  * The type Pessoa eleitora.
  */
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  /**
+   * Instantiates a new Pessoa eleitora.
+   *
+   * @param nome   o nome
+   * @param cpf o CPF da pessoa eleitora
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    setNome(nome);
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
