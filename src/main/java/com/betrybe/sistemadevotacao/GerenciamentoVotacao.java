0b914f45f594fb3a5e1ea986db23b26a80fb5c4e
Geovanna Otoni <geovannaotoni@gmail.com>
2023-11-24 17:49:47 -0300

    feat05: Métodos cadastrarPessoaCandidata e cadastrarPessoaEleitora da classe GerenciamentoVotacao

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -6,18 +6,36 @@ import java.util.ArrayList;
  * The type Gerenciamento votacao.
  */
 public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
-  private ArrayList<PessoaCandidata> pessoasCandidatas;
-  private ArrayList<PessoaEleitora> pessoasEleitoras;
-  private ArrayList<String> cpfsComputados;
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<PessoaEleitora>();
+  private ArrayList<String> cpfsComputados = new ArrayList<String>();
 
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
-
+    for (PessoaCandidata pessoa : pessoasCandidatas) {
+      // utiliza-se o == para comparar igualdade entre tipos primitivos
+      // compara-se com base no endereço de memória
+      if (pessoa.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
+    }
+    PessoaCandidata pessoaCandidata = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(pessoaCandidata);
   }
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
-
+    for (PessoaEleitora pessoa : pessoasEleitoras) {
+      // utiliza-se o equals porque String é um tipo não primitivo
+      // compara-se o conteúdo do objeto
+      if (pessoa.getCpf().equals(cpf)) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+      }
+    }
+    PessoaEleitora pessoaEleitora = new PessoaEleitora(nome, cpf);
+    pessoasEleitoras.add(pessoaEleitora);
   }
 
   @Override
